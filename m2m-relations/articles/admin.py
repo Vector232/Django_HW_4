b3085522beb3f3453e372761fd28ca70b12365d8
Vlad Troyan <030705vt@mail.ru>
2023-09-29 15:45:40 +0300

    task 2 copleted

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+
+class ScopeInlineFormSet(BaseInlineFormSet):
+    def clean(self) -> None:
+        counter = 0
+        
+        for form in self.forms:
+            counter += 1 if form.cleaned_data.get('is_main') else 0
+
+        if counter == 0:
+            raise ValidationError('Укажите основной раздел!')
+        elif counter > 1:
+            raise ValidationError('Основным может быть только один раздел!')
+        
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormSet
+    extra = 0
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline,]
--- m2m-relations/website/settings.py
+++ m2m-relations/website/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'articles.apps.ArticlesConfig'
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_m2m_relations',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -129,3 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
